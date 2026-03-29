1bd703f51fd0953d111e20b50481cce57eaf8a30
cryptocreeper94 <jason@livfitoken.com>
2026-03-28 23:20:59 -0500

    Initialize Express config to fix Render Web Service deployment

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the repository root (images, etc)
+app.use(express.static(path.join(__dirname, '.')));
+
+// Catch-all to serve the main index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`TLID Registry running on port ${PORT}`);
+});
